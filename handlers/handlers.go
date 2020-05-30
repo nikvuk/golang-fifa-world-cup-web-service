45c7843c63c55efa4d4ada0f39b2b814f8aa616b
Nik V <nvukovlj@gmail.com>
2020-05-30 11:41:35 +0100

    Minor update to Module 2

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,8 +14,8 @@ func RootHandler(res http.ResponseWriter, req *http.Request) {
 func ListWinners(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "application/json")
 	year := req.URL.Query().Get("year")
-	winners, err := data.ListAllJSON()
 	if year == "" {
+		winners, err := data.ListAllJSON()
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
 			return
